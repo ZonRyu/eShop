2a0d29df9c5d589222ee4e9244986b075352f7cb
ZonRyu <julyonchang@gmail.com>
2024-09-12 23:01:21 +0700

    route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
