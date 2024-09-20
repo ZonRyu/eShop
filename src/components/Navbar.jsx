eaba29b8c24c10f19b61009fa1676a83a5a149f4
ZonRyu <julyonchang@gmail.com>
2024-09-20 14:43:59 +0700

    little design for navbar and cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const products = useSelector((state) => state.cart.products)
   return (
     <nav className='bg-white shadow-md'>
-      <div className='container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center'>
+      <div className='container mx-auto xs:px-4 py-4 flex justify-between items-center'>
         <div className='text-lg font-bold'>
           <Link to="/">e-Commerce</Link>
         </div>
